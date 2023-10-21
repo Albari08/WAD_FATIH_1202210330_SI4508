a0ef0a7b7026689131387763dbcf8a39741a33c7
Albari08 <fatihalbari@student.telkomuniversity.ac.id>
2023-10-21 15:12:11 +0700

    Tugas 2

--- Tugas 2 - Pengenalan Server Side & Dasar PHP/plain/controllers/move_image.php
+++ Tugas 2 - Pengenalan Server Side & Dasar PHP/plain/controllers/move_image.php
@@ -6,28 +6,16 @@ $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
 $check = getimagesize($_FILES["image"]["tmp_name"]);
     if ($check) {
-        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
         $uploadOk = 0;
     }
 
-// Check file size
-if ($_FILES["image"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+if ($uploadOk == 1) {
+    move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile);
     // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-        echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+    echo "Sorry, there was an error uploading your file.";
 }
+
 ?>
